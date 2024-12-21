f1360c50c13e9c61056d8f670df0e3b6981c6c24
Regeneric <hubek916@gmail.com>
2024-12-22 00:55:21 +0100

    Init script for dev env setup

--- prepare_dev_env.sh
+++ prepare_dev_env.sh
@@ -113,7 +113,8 @@ docker-compose -f docker/docker-compose.yml up -d reverse_proxy
 
 current_date=$(date +%Y%m%d)
 echo "Creating simple local DNS zone..."
-cat << EOF > bind9/db.${domain_name}
+zone_name="db.${domain_name}"
+cat << EOF > bind9/${domain_name}
 \$TTL 300
 @       IN     SOA    ns1.${domain_name}. root.${domain_name}. (
                        ${current_date}00 ; serial
