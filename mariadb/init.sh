83dbba9542e99f13739890663f36431375bfe37c
hkk <hubek916@gmail.com>
2024-12-27 17:02:08 +0100

    Prepare script changes

--- mariadb/init.sh
+++ mariadb/init.sh
@@ -5,3 +5,7 @@ source ../docker/.env
 docker exec mariadb mariadb -u root -p"${SQL_ROOT_PASS}" -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASS}';"
 docker exec mariadb mariadb -u root -p"${SQL_ROOT_PASS}" -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%';"
 docker exec mariadb mariadb -u root -p"${SQL_ROOT_PASS}" -e "FLUSH PRIVILEGES;"
+
+read -p "MariaDB database name: " database_name
+
+docker exec mariadb mariadb -u root -p"${SQL_ROOT_PASS}" -e "CREATE DATABASE ${database_name};"
--- mongodb/init.sh
+++ mongodb/init.sh
@@ -6,3 +6,11 @@ docker exec mongodb mongosh --authenticationDatabase admin -u "$MONGO_USER" -p "
 docker exec mongodb mongosh --authenticationDatabase "$MONGO_INITDB_NAME" -u "$MONGO_INITDB_USER" -p "$MONGO_PASS" --eval "$(cat init/create_role.js)" admin
 docker exec mongodb mongosh --authenticationDatabase "$MONGO_INITDB_NAME" -u "$MONGO_INITDB_USER" -p "$MONGO_PASS" --eval "$(cat init/create_root.js)" admin
 docker exec mongodb mongosh --authenticationDatabase "$MONGO_INITDB_NAME" -u "$MONGO_INITDB_USER" -p "$MONGO_PASS" --eval "$(cat init/grant_role.js)"  admin
+
+read -p "MongoDB database name: " database_name
+
+cat << EOF > init/create_database.js
+db = db.getSiblingDB('${database_name}');
+EOF
+
+docker exec mongodb mongosh --authenticationDatabase "$MONGO_INITDB_NAME" -u "$MONGO_INITDB_USER" -p "$MONGO_PASS" --eval "$(cat init/create_database.js)"
